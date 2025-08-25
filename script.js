ed75951bd55c0272941fea535cc29397ea160974
Cassandra Mzola <cassandramzola@gmail.com>
2025-08-25 14:50:52 +0200

    Initial commit of Green Digital Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Reveal each section when scrolling into view
+ScrollReveal().reveal('.section', {
+  duration: 1000,
+  distance: '50px',
+  easing: 'ease-in-out',
+  origin: 'bottom',
+  reset: false
+});
